8024521c9747fae4d0e7b3eab8a570e6950d5caa
Dania Rinker <daniarinker@gmail.com>
2021-02-27 01:42:36 -0800

    function breakdown

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  // function for generating today stuff
+  generateToday();
+  create;
+});
+
+// function for time blocks
+generateTimeBlock();
+
+// function update the time
+functionUpdateTime();
+
+// saving to local storage
+updateTimeRow();
+
+// save the data
+renderData();
+
+$("button").on("click");
+})
+
+function generateToday() {
+  let today = moment().format('')
+
+}
